6ab7382d7974d3d527a90e88dbfe0621cc2cc5de
StasGotsko <36699096+StasGotsko@users.noreply.github.com>
2018-02-25 15:07:13 +0200

    hww
    hw2

---
+++ Hw2/scripts/script.js
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<table border="1" cellpadding="center" style="width:50%" >
+		<caption> My Table</caption>
+		<tr align="center">
+			<th>ID</th>	
+			<th>Fname</th>
+			<th>Lname</th>
+			<th>Age</th>
+		</tr>
+		<tr align="center">
+			<td>ID</td>	
+			<td>Fname</td>
+			<td>Lname</td>
+			<td>Age</td>
+		</tr>
+	</table>
+	<p>Lorem ipsum <big>dolor</big> sit amet, consectetur adipisicing elit. Quos minima nesciunt accusantium dolores reiciendis dolorum. Quibusdam quasi perferendis at mollitia nobis, <small>dignissimos</small>, officiis <em>reiciendis</em>, dolorum modi, <b>ducimus</b> dolorem esse numquam.
+	</p>
+	<h1>Hello</h1>
+	<h4>Hello</h4>
+	<h6>Hello</h6>
+	<span>Hello</span>
+	<br>
+	<select>
+		<option selected="">
+			jhghgh		
+		</option>
+		<option selected="">
+			hjghj
+		</option>
+		<option selected="">
+			hjgh
+		</option>
+		<option selected="">
+			jghjh
+		</option>
+	</select>
+	<ul>
+		<li>
+			kjkjfjsafkldsfjkl
+		</li>
+		<li>
+			kjkdhfkjdskjfh
+		</li>
+	</ul>
+	<br>
+	<ol>
+		<li>
+			kjkjfjsafkldsfjkl
+		</li>
+		<li>
+			kjkdhfkjdskjfh
+		</li>
+	</ol>
+
+	<textarea>hjghjhjkhkjhkjhjk</textarea> <br>
+
+	<progress value=25" max="36"> </progress><br>
+	<button>hjhjhggjh</button><br>
+</body>
+</html>
